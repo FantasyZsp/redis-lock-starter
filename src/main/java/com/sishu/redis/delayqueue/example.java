12eec302cd1603ed9a4dfa853b28fbefe7c6351b
zsp-laptop <541054738@qq.com>
2020-05-15 00:36:46 +0800

    test: lmstfy

---
+++ src/main/java/com/sishu/redis/delayqueue/example.java
@@ -0,0 +1,127 @@
+package com.sishu.redis.delayqueue;
+
+import com.meitu.platform.lmstfy.Job;
+import com.meitu.platform.lmstfy.client.LmstfyClient;
+import com.meitu.platform.lmstfy.exception.LmstfyException;
+import com.meitu.platform.lmstfy.exception.LmstfyNotJobException;
+import com.meitu.platform.lmstfy.response.DeadLetterResponse;
+
+/**
+ * Description:
+ *
+ * @author Yesphet
+ * @date 2019-12-06
+ */
+public class example {
+
+  private static String token = "01DVCJBB8XZTB73SSDCZ4Q6VZN";
+  private static String host = "192.168.0.101";
+  private static int port = 7777;
+  private static String namespace = "sdk-test";
+  private static String queue = "sdk-test-queue";
+  private static LmstfyClient client = new LmstfyClient(host, port, namespace, token);
+
+  public static void main(String[] args) {
+    publish();
+    consume();
+    deleteAndAck();
+    queueSize();
+    peekQueue();
+    peekJob();
+    peekDeadLetter();
+    respawnDeadLetter();
+  }
+
+  private static void publish() {
+    try {
+      String jobID = client.publish(queue, "test data".getBytes(), 5, (short) 2, 0);
+      System.out.println(jobID);
+    } catch (LmstfyException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private static void consume() {
+    try {
+      Job job = client.consume(3, 2, queue);
+      System.out.println(job.getData());
+    } catch (LmstfyNotJobException e) {
+      System.out.println("There has no available job");
+    } catch (LmstfyException e) {
+      System.out.println("Request Lmstfy error, " + e.getMessage());
+    }
+  }
+
+  private static void deleteAndAck() {
+    try {
+      String jobID1 = client.publish(queue, "test data".getBytes(), 5, (short) 2, 0);
+      client.delete(queue, jobID1);
+      String jobID2 = client.publish(queue, "test data".getBytes(), 5, (short) 2, 0);
+      client.ack(queue, jobID2);
+    } catch (LmstfyException e) {
+      System.out.println("Request Lmstfy error, " + e.getMessage());
+    }
+  }
+
+  private static void queueSize() {
+    try {
+      int size1 = client.queueSize(queue);
+      client.publish(queue, "test data".getBytes(), 5, (short) 2, 0);
+      int size2 = client.queueSize(queue);
+      System.out.printf("%d %d\n", size1, size2);
+    } catch (LmstfyException e) {
+      System.out.println("Request Lmstfy error, " + e.getMessage());
+    }
+  }
+
+  private static void peekQueue() {
+    try {
+      Job job = client.peekQueue(queue);
+      if (job == null) {
+        System.out.println("There has no available job to peek");
+        return;
+      }
+      if (job.getData() == null) {
+        System.out.printf("The job %s is expired\n", job.getJobID());
+      } else {
+        System.out.println(job.getData());
+      }
+    } catch (LmstfyException e) {
+      System.out.println("Request Lmstfy error, " + e.getMessage());
+    }
+  }
+
+  private static void peekJob() {
+    try {
+      String jobID = client.publish(queue, "test data".getBytes(), 5, (short) 2, 0);
+      Job job = client.peekJob(queue, jobID);
+      if (job == null) {
+        System.out.println("There has no available job");
+      } else {
+        System.out.printf("%s\n", job.getData());
+      }
+    } catch (LmstfyException e) {
+      System.out.println("Request Lmstfy error, " + e.getMessage());
+    }
+  }
+
+  private static void peekDeadLetter() {
+    try {
+      DeadLetterResponse deadLetterResponse = client.peekDeadLetter(queue);
+      System.out.println(deadLetterResponse.getDeadLetterSize());
+    } catch (LmstfyException e) {
+      System.out.println("Request Lmstfy error, " + e.getMessage());
+    }
+  }
+
+  private static void respawnDeadLetter() {
+    try {
+      int count = client.respawnDeadLetter(queue, 1, 60);
+      System.out.println("Respawn deadletter count: " + count);
+    } catch (LmstfyException e) {
+      System.out.println("Request Lmstfy error, " + e.getMessage());
+    }
+  }
+
+
+}
