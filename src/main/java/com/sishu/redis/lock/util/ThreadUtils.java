4f50d2e5eb9518c75c6bfa3a6ec2c4dff33ff63d
zhaoshipan <541054738@qq.com>
2020-05-25 15:36:20 +0800

    utils: 线程工具。

--- src/main/java/com/sishu/redis/lock/util/ThreadUtils.java
+++ src/main/java/com/sishu/redis/lock/util/ThreadUtils.java
@@ -1,5 +1,7 @@
 package com.sishu.redis.lock.util;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author ZSP
  */
@@ -12,8 +14,19 @@ public class ThreadUtils {
     }
   }
 
+  public static void sleepSeconds(long timeout) {
+    try {
+      TimeUnit.SECONDS.sleep(timeout);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
+
+
   public static void startAndJoin(Thread thread, long mills) {
-    thread.start();
+    if (!thread.isAlive()) {
+      thread.start();
+    }
     try {
       thread.join(mills);
     } catch (InterruptedException e) {
@@ -21,7 +34,26 @@ public class ThreadUtils {
     }
   }
 
+  public static void startAndJoin(Thread thread) {
+    if (!thread.isAlive()) {
+      thread.start();
+    }
+    try {
+      thread.join();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
+
   public static void start(Thread thread) {
     thread.start();
+  }
+
+  public static void join(Thread thread) {
+    try {
+      thread.join();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
   }
 }
