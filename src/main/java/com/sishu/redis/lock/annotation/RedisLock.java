a0655269414e1b8381d15a7d6a5bf92180f47681
ZSP <541054738@qq.com>
2020-12-01 11:07:41 +0800

    fix: 注释修复

--- src/main/java/com/sishu/redis/lock/annotation/RedisLock.java
+++ src/main/java/com/sishu/redis/lock/annotation/RedisLock.java
@@ -52,6 +52,7 @@ public @interface RedisLock {
   /**
    * 尝试获取锁的等待时间
    * 小于0时一直阻塞等待，大于等于零时当超过指定时间会进入获取锁失败流程。
+   * 单位 {@link RedisLock#timeUnit()}
    */
   long waitTime() default -1;
 
@@ -60,6 +61,7 @@ public @interface RedisLock {
    * 请合理预估业务需要时间，防止锁提前被释放。
    * -1表明线程在占有锁期间不会因为执行时间过长导致锁释放。
    * 正数代表在指定时间后将自动释放锁。
+   * 单位 {@link RedisLock#timeUnit()}
    */
   long leaseTime() default -1;
 
@@ -70,7 +72,7 @@ public @interface RedisLock {
   String exceptionMessage() default "服务器繁忙，请稍后重试！";
 
   /**
-   * 主键间排序顺序。默认都是0，即默认将会按照声明的顺序依次处理注解内部key。
+   * 注解间排序顺序。默认都是0，即默认将会按照声明的顺序依次处理注解内部key。
    */
   int order() default 0;
 }
